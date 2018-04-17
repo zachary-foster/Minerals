7945b3b17bcc48c8940090df9565a468b58d7236
zachary-foster <zacharyfoster1989@gmail.com>
2018-04-16 22:45:59 -0700

    drafted growth rate code

--- Source/Minerals/DynamicMineral.cs
+++ Source/Minerals/DynamicMineral.cs
@@ -17,120 +17,85 @@ namespace Minerals
     /// <permission>No restrictions</permission>
     public class DynamicMineral : StaticMineral
     {
+
+        public ThingDef_DynamicMineral attributes
+        {
+            get
+            {
+                return this.def as ThingDef_DynamicMineral;
+            }
+        }
+
+
         // ======= Growth rate factors ======= //
 
-        public float temperatureGrowthRateFactor
+        public float growthRateFactor(growthRateModifier mod)
+        {
+            float myValue = mod.value(this);
+            
+            // decays if too high or low
+            float stableRangeSize = mod.maxStable - mod.minStable;
+            if (myValue > mod.maxStable) 
+            {
+                return - mod.aboveMaxDecayRate * (myValue - mod.maxStable) / stableRangeSize;
+            } 
+            if (myValue < mod.minStable) 
+            {
+                return - mod.belowMinDecayRate * (mod.minStable - myValue) / stableRangeSize;
+            }
+            
+            // does not grow if too high or low
+            else if (myValue < mod.minGrow || myValue > mod.maxGrow) 
+            {
+                return 0f;
+            } 
+            
+            // slowed growth if too high or low
+            if (myValue < mod.idealGrow)
+            {
+                return 1f - ((mod.idealGrow - myValue) / (mod.idealGrow - mod.minGrow));
+            }
+            else 
+            {
+                return 1f - ((myValue - mod.idealGrow) / (mod.maxGrow - mod.idealGrow));
+            }
+            
+            // just right!
+            return 1f;
+        }
+
+        public List<float> allGrowthRateFactors 
         {
             get
             {
-                float temperature = this.Position.GetTemperature(this.Map);
-                if (temperature > this.attributes.maxStableTemperature) // melts if too hot
-                {
-                    return - this.attributes.meltRate * (temperature - this.attributes.maxStableTemperature);
-                } 
-                else if (temperature < this.attributes.minGrowTemperature || temperature > this.attributes.maxGrowTemperature)
-                {
-                    return 0f;
-                } 
-                else if (temperature < this.attributes.idealGrowTemperature) // grows slower in extreme cold
-                {
-                    return 1f - ((this.attributes.idealGrowTemperature - temperature) / (this.attributes.idealGrowTemperature - this.attributes.minGrowTemperature));
-                }
-                else // grows slower when too warm
-                {
-                    return 1f - ((temperature - this.attributes.idealGrowTemperature) / (this.attributes.maxGrowTemperature - this.attributes.idealGrowTemperature));
-                }
-
+                this.attributes.allRateModifiers.Select(mod => growthRateFactor(mod));
             }
         }
 
-
-        public float weatherGrowthRateFactor
-        {
-            get
-            {
-                if (this.Position.Roofed(this.Map) || this.Map.weatherManager.curWeather.rainRate < 0.05)
-                {
-                    return 1;
-                }
-                else 
-                {
-                    return -1 * this.Map.weatherManager.curWeather.rainRate * this.attributes.dissolveRate;
-                }
-            }
-        }
-
-
-        public float sizeGrowthRateFactor
-        {
-            get
-            {
-                if (this.growthInt >= 1)
-                {
-                    return 0.01f; // Needs to be above 0 to dissolve
-                }
-                else
-                {
-                    return (1.1f - this.growthInt) / 1.1f;
-                }
-            }
-        }
-
-
-        public float lightGrowthRateFactor
-        {
-            get
-            {
-                float light = this.Map.glowGrid.GameGlowAt(this.Position);
-                if (light >= this.attributes.minGrowLight)
-                {
-                    return 1 + light * this.attributes.lightBoostFactor;
-                }
-                else
-                {
-                    return 0f;
-                }
-            }
-        }
-
-        public float waterOffsetRateFactor
-        {
-            get
-            {
-                if (this.attributes.waterOffsetGrowthEffect)
-                {
-                    if (this.myDistToWater >= 0 && this.myDistToWater <= this.attributes.waterOffsetRadius)
-                    {
-                        return (float)Math.Pow((this.attributes.waterOffsetRadius + 1 - this.myDistToWater) / this.attributes.waterOffsetRadius, 3d);
-                    }
-                    else
-                    {
-                        return 0f;
-                    }
-
-                }
-                else
-                {
-                    return 1f;
-                }
-            }
-        }
 
         public override float GrowthRate
         {
             get
             {
-                if (this.temperatureGrowthRateFactor < 0) // If melting from heat
+                // Get growth rate factors
+                List<float> rateFactors = this.allGrowthRateFactors;
+                List<float> positiveFactors = rateFactors.FindAll(fac => fac >= 0);
+                List<float> negativeFactors = rateFactors.FindAll(fac => fac < 0);
+
+                // if any factors are negative, add them together and ignore positive factors
+                if (negativeFactors.Count > 0)
                 {
-                    return this.temperatureGrowthRateFactor;
+                    return negativeFactors.Sum();
                 }
-                else if (this.weatherGrowthRateFactor < 0) // If melting from rain
+
+                // if all positive, multiply them
+                if (positiveFactors.Count > 0)
                 {
-                    return this.weatherGrowthRateFactor * this.sizeGrowthRateFactor * this.temperatureGrowthRateFactor;
-                } else // growing normally
-                {
-                    return this.temperatureGrowthRateFactor * this.sizeGrowthRateFactor * this.lightGrowthRateFactor * this.weatherGrowthRateFactor * this.waterOffsetRateFactor;
+                    return positiveFactors.Aggregate(1f, (acc, val) => acc * val);
                 }
+
+                // If there are no growth rate factors, grow at full speed
+                return 1f;
             }
         }
 
@@ -138,7 +103,7 @@ namespace Minerals
         {
             get
             {
-                float growthPerTick = (1f / (GenDate.TicksPerDay * this.def.plant.growDays));
+                float growthPerTick = (1f / (GenDate.TicksPerDay * this.attributes.growDays));
                 return growthPerTick * this.GrowthRate;
             }
         }
@@ -156,23 +121,95 @@ namespace Minerals
     /// <permission>No restrictions</permission>
     public class ThingDef_DynamicMineral : ThingDef_StaticMineral
     {
-        // Temperature effects on growth rate
-        public float aboveMaxEvaporateRate = 1f;  // How quickly it melts when above maxStableTemperature
-        public int maxStableTemperature = 200; // Will evaporate above this temperature
-        public int maxGrowTemperature = 100; // Will not grow above this temperature
-        public int idealGrowTemperature = 50; // Grows fastest at this temperature
-        public int minGrowTemperature = 0; // Will not grow below this temperature
-        public int minStableTemperature = -1000; // Will evaporate below this temperature
-        public float belowMinEvaporateRate = 1f;  // How quickly it melts when below minStableTemperature
-         
-        // Weather effects on growth rate
-        public float rainDissolveRate = 0f; // How quickly it dissolves in rain
-        public float lightBoostFactor = 0f; // How effective light is for speeding formation
-        public float minGrowLight = 0f; // Must have this much light to grow
+        public float growDays = 100f;
+        public tempGrowthRateModifier tempGrowthRateModifer;  // Temperature effects on growth rate
+        public rainGrowthRateModifier rainGrowthRateModifer;  // Rain effects on growth rate
+        public lightGrowthRateModifier lightGrowthRateModifer; // Light effects on growth rate
+        public fertGrowthRateModifier fertGrowthRateModifer;  // Fertility effects on growth rate
+        public distGrowthRateModifier distGrowthRateModifer;  // Distance to needed terrain effects on growth rate
+        public sizeGrowthRateModifier sizeGrowthRateModifer;  // Current size effects on growth rate
 
-        // Fertility effects of growth rate
-        public float minFertility = 0.0f;
-        public float maxFertility = 999f;
 
+        public List<growthRateModifier> allRateModifiers 
+        {
+            get 
+            {
+                List<growthRateModifier> output = new List<growthRateModifier>{
+                    tempGrowthRateModifer,
+                    rainGrowthRateModifer,
+                    lightGrowthRateModifer,
+                    fertGrowthRateModifer,
+                    distGrowthRateModifer,
+                    sizeGrowthRateModifer
+                };
+                output.RemoveAll(item => item == null);
+                return output;
+            }
+        }
+    }
+
+
+    public class growthRateModifier
+    {
+        public bool active; // This modifier only has an effect when true
+        public float aboveMaxDecayRate;  // How quickly it decays when above maxStableFert
+        public float maxStable; // Will decay above this fertility level
+        public float maxGrow; // Will not grow above this fertility level
+        public float idealGrow; // Grows fastest at this fertility level
+        public float minGrow; // Will not grow below this fertility level
+        public float minStable; // Will decay below this fertility level
+        public float belowMinDecayRate;  // How quickly it decays when below minStableFert
+
+        public virtual float value(DynamicMineral aMineral);
+    }
+
+    public class tempGrowthRateModifier : growthRateModifier
+    {
+        public override float value(DynamicMineral aMineral)
+        {
+            return aMineral.Position.GetTemperature(aMineral.Map);
+        }
+    }
+
+    public class rainGrowthRateModifier : growthRateModifier
+    {
+        public override float value(DynamicMineral aMineral)
+        {
+            return aMineral.Map.weatherManager.curWeather.rainRate;
+        }
+    }
+
+    public class lightGrowthRateModifier : growthRateModifier
+    {
+        public override float value(DynamicMineral aMineral)
+        {
+            return aMineral.Map.glowGrid.GameGlowAt(aMineral.Position);
+        }
+    }
+
+
+    public class fertGrowthRateModifier : growthRateModifier
+    {
+        public override float value(DynamicMineral aMineral)
+        {
+            return aMineral.Map.fertilityGrid.FertilityAt(aMineral.Position);
+        }
+    }
+
+    public class distGrowthRateModifier : growthRateModifier
+    {
+        public override float value(DynamicMineral aMineral)
+        {
+            return aMineral.distFromNeededTerrain;
+        }
+    }
+
+
+    public class sizeGrowthRateModifier : growthRateModifier
+    {
+        public override float value(DynamicMineral aMineral)
+        {
+            return aMineral.size;
+        }
     }
 }
